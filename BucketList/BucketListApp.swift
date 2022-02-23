6c0e5276f6ee3812c70c2f0df689b2cad1fa36c3
Dom Bryan <dombguitar@gmail.com>
2022-02-23 11:04:17 +0000

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Dom Bryan on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
